8f1680b8422c962c08a63043f6f730ce1771711b
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2020-10-13 07:32:43 -0700

    Added validation testing

---
+++ WhiteboardModel/src/ComputerValidationTest.java
@@ -0,0 +1,13 @@
+/*
+ * Here, we do validation testing for Computer.
+ */
+class ComputerValidationTest
+{
+    public static void main(String[] args)
+    {
+        // Test left of, on, and right of boundaries
+        Computer comp1 = new Computer();
+        System.out.println(comp1);
+        
+    }
+}
